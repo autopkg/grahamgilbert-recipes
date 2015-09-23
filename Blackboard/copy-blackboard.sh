3c7af6dfadc0f6530cdbbe0c512e9916c13b9ec9
Graham Gilbert <graham@grahamgilbert.com>
2015-09-23 11:56:58 +0100

    Yeah, better upload the actual script

--- Blackboard/copy-blackboard.sh
+++ Blackboard/copy-blackboard.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
+exists=false
+
 # Does it exist already?
-
-# Is it the right version?
-
-# if Not, remove it
+app="/Library/Management/BlackboardCollaborate/Blackboard Collaborate Launcher.app"  
+userapp="~/Applications/Blackboard Collaborate Launcher.app"
+installedversion=`/usr/bin/defaults read "$app/Contents/Info.plist" CFBundleShortVersionString`
+  # /   (root directory)
+if [ -e "$userapp" ]
+then
+  # App exists in homedir, make sure it's the right version
+  userversion=`/usr/bin/defaults read "$userapp/Contents/Info.plist" CFBundleShortVersionString`
+    # Is it the right version?
+    if [ "$userversion" == "$installedversion"]
+        then
+        exists=true
+    else
+        # if Not, remove it
+        /bin/rm -rf $userapp
+    fi
+fi
 
 # if doesn't exist, copy it
+if [ "$exists" = false ]
+    then
+    /bin/mkdir -p ~/Applications
+    /bin/cp -R "$app" ~/Applications
+fi
